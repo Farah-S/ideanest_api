f00723561d0acacf56f545740c11dcc06cf7ccf9
Farah Sherif <farah.faroha132@gmail.com>
2024-02-08 19:35:16 +0200

    Update codebase to use MongoDB and Redis

---
+++ config/default.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+// ? Struct Config
+type Config struct {
+	DBUri    string `mapstructure:"MONGODB_LOCAL_URI"`
+	RedisUri string `mapstructure:"REDIS_URL"`
+	Port     string `mapstructure:"PORT"`
+}
+
+// The function `LoadConfig` loads a configuration file from the specified path and unmarshals it into
+// a struct.
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigType("env")
+	viper.SetConfigName("app")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
+
