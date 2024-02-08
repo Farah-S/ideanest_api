b0916967b4c76f6df669da6cf13bfc3f380b348a
Farah Sherif <farah.faroha132@gmail.com>
2024-02-08 20:08:35 +0200

    added basic html

--- cmd/main.go
+++ cmd/main.go
@@ -85,12 +85,29 @@ func main() {
 	} else if err != nil {
 		panic(err)
 	}
-
-	router := server.Group("/api")
-	router.GET("/healthchecker", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": value})
+	print(value)
+	router := gin.Default()
+	router.LoadHTMLGlob("pkg/api/pages/*")
+	router.GET("/api", func(c *gin.Context) {
+	
+	// Call the HTML method of the Context to render a template
+	c.HTML(
+		// Set the HTTP status to 200 (OK)
+		http.StatusOK,
+		// Use the index.html template
+		"index.html",
+		// Pass the data that the page uses (in this case, 'title')
+		gin.H{
+			"title": "Home Page",
+		},
+	)
+	// c.JSON(http.StatusOK, gin.H{"status": "success", "message": value})
 	})
-
+	// apirouter := server.Group("/api")
+	// apirouter.GET("/healthchecker", func(ctx *gin.Context) {
+	// 	ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": value})
+	// })
+	router.Run()
 	log.Fatal(server.Run(":" + config.Port))
 }
 
